4bf353329c8bb2f26f01c62058521596c1f1ff1e
brooke_zb <brooke_zb@qq.com>
2021-12-17 21:02:57 +0800

    complete authority verify

--- bhs-common/src/main/java/top/brookezb/bhs/aspect/AuthenticationAspect.java
+++ bhs-common/src/main/java/top/brookezb/bhs/aspect/AuthenticationAspect.java
@@ -29,24 +29,54 @@ public class AuthenticationAspect {
     /**
      * 权限鉴定切点
      */
-    @Pointcut("@annotation(top.brookezb.bhs.annotation.RequirePermission)")
-    public void requirePermission() {
+    @Pointcut("@within(top.brookezb.bhs.annotation.RequirePermission) || @within(top.brookezb.bhs.annotation.RequireAuth) || @within(top.brookezb.bhs.annotation.PermitAll)")
+    public void auth() {
     }
 
-    @Around("requirePermission()")
+    @Around("auth()")
     public Object aroundRequirePermission(ProceedingJoinPoint joinPoint) throws Throwable {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
+        Class<?> clazz = method.getDeclaringClass();
 
         // 优先获取方法上的注解信息
-        RequirePermission requirePermissionAnno = method.getAnnotation(RequirePermission.class);
+        // 处理权限鉴定
+        if (requirePermission(method.getAnnotation(RequirePermission.class))) {
+            return joinPoint.proceed();
+        }
+        // 处理登录鉴定
+        if (requireAuth(method.getAnnotation(RequireAuth.class))) {
+            return joinPoint.proceed();
+        }
+        // 处理放行
+        if (permitAll(method.getAnnotation(RequireAuth.class))) {
+            return joinPoint.proceed();
+        }
 
         // 方法上获取不到，获取类上的注解信息
-        if (requirePermissionAnno == null) {
-            requirePermissionAnno = method.getDeclaringClass().getAnnotation(RequirePermission.class);
+        // 处理权限鉴定
+        if (requirePermission(clazz.getAnnotation(RequirePermission.class))) {
+            return joinPoint.proceed();
         }
-        String[] requirePermissions = requirePermissionAnno.values();
-        RequirePermission.Relation relation = requirePermissionAnno.relation();
+        // 处理登录鉴定
+        if (requireAuth(clazz.getAnnotation(RequireAuth.class))) {
+            return joinPoint.proceed();
+        }
+
+        return joinPoint.proceed();
+    }
+
+    /**
+     * 权限鉴定
+     * @param anno 注解
+     * @return true: 通过鉴定； false: 没有该注解
+     */
+    public boolean requirePermission(RequirePermission anno) {
+        if (anno == null) {
+            return false;
+        }
+        String[] requirePermissions = anno.values();
+        RequirePermission.Relation relation = anno.relation();
 
         // 获取session中的权限信息
         HttpSession session = ServletUtils.getSession();
@@ -59,7 +89,7 @@ public class AuthenticationAspect {
         if (relation == RequirePermission.Relation.OR) {
             for (String requirePermission : requirePermissions) {
                 if (cur_user.getRole().getPermissions().contains(requirePermission)) {
-                    return joinPoint.proceed();
+                    return true;
                 }
             }
             throw new ForbiddenException("您没有权限进行此操作");
@@ -70,24 +100,21 @@ public class AuthenticationAspect {
                     throw new ForbiddenException("您没有权限进行此操作");
                 }
             }
-            return joinPoint.proceed();
+            return true;
         }
     }
 
-    /**
-     * 登录鉴定切点
-     */
-    @Pointcut("@annotation(top.brookezb.bhs.annotation.RequireAuth)")
-    public void requireAuth() {
-    }
-
-    @Around("requireAuth()")
-    public Object aroundRequireAuth(ProceedingJoinPoint joinPoint) throws Throwable {
-        // 获取session中的用户信息
+    public boolean requireAuth(RequireAuth anno) {
+        // 获取session中的权限信息
         HttpSession session = ServletUtils.getSession();
-        if (session.getAttribute("loginUser") == null) {
+        User cur_user = (User) session.getAttribute("loginUser");
+        if (cur_user == null) {
             throw new AuthenticationException("请登录后再操作");
         }
-        return joinPoint.proceed();
+        return true;
+    }
+
+    public boolean permitAll(RequireAuth anno) {
+        return anno != null;
     }
 }
