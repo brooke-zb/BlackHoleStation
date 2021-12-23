a9be5fdc6139fa61222d5805c60cb9bee89f4834
brooke_zb <brooke_zb@qq.com>
2021-12-23 16:33:04 +0800

    optimize csrf protection

--- bhs-common/src/main/java/top/brookezb/bhs/interceptor/CsrfInterceptor.java
+++ bhs-common/src/main/java/top/brookezb/bhs/interceptor/CsrfInterceptor.java
@@ -5,6 +5,8 @@ import org.apache.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.HandlerInterceptor;
 import top.brookezb.bhs.constant.AppConstants;
+import top.brookezb.bhs.exception.AuthenticationException;
+import top.brookezb.bhs.exception.ForbiddenException;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -21,14 +23,18 @@ import java.util.UUID;
 public class CsrfInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception {
+        // 不拦截GET和OPTIONS请求
+        if (request.getMethod().equals("GET") || request.getMethod().equals("OPTIONS")) {
+            return true;
+        }
+
         // 获取前端提交的token
         String token = request.getHeader(AppConstants.CSRF_HEADER);
 
         // 未登录或无token则返回403
         HttpSession session = request.getSession();
-        if (session.getAttribute("user") == null || token == null) {
-            setFailResponse(response);
-            return false;
+        if (session.getAttribute(AppConstants.SESSION_USER_KEY) == null || token == null) {
+            throw new ForbiddenException("未通过CSRF检测");
         }
 
         // 获取服务端存储的token
@@ -38,9 +44,8 @@ public class CsrfInterceptor implements HandlerInterceptor {
             return true;
         }
 
-        setFailResponse(response);
         putCSRFToken(request, response);
-        return false;
+        throw new ForbiddenException("未通过CSRF检测");
     }
 
     /**
@@ -59,16 +64,5 @@ public class CsrfInterceptor implements HandlerInterceptor {
         Cookie csrfToken = new Cookie(AppConstants.CSRF_HEADER, token);
         csrfToken.setPath("/");
         response.addCookie(csrfToken);
-    }
-
-    /**
-     * CSRF校检不通过，返回403
-     * @param response 响应
-     * @throws Exception IO流异常
-     */
-    private void setFailResponse(HttpServletResponse response) throws Exception {
-        response.setStatus(HttpStatus.SC_FORBIDDEN);
-        response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write("{\"success\": false, \"data\": null,\"msg\": \"未通过CSRF检测\"}");
     }
 }
