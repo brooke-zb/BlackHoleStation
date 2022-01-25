c3595bce849438ebf528f453fa33e2698ec31fc3
brooke_zb <brooke_zb@qq.com>
2022-01-26 01:30:37 +0800

    complete role service

---
+++ bhs-app/src/main/java/top/brookezb/bhs/service/RoleServiceImpl.java
@@ -0,0 +1,77 @@
+package top.brookezb.bhs.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import top.brookezb.bhs.exception.InvalidException;
+import top.brookezb.bhs.exception.NotFoundException;
+import top.brookezb.bhs.mapper.RoleMapper;
+import top.brookezb.bhs.model.Role;
+
+import java.util.List;
+
+/**
+ * @author brooke_zb
+ */
+@Service
+@AllArgsConstructor
+public class RoleServiceImpl implements RoleService {
+    private RoleMapper roleMapper;
+
+    @Override
+    public Role selectById(Long rid) {
+        Role role = roleMapper.selectById(rid);
+        if (role != null) {
+            return role;
+        }
+        throw new NotFoundException("未找到该角色");
+    }
+
+    @Override
+    public List<Role> selectAll() {
+        return roleMapper.selectAll();
+    }
+
+    @Override
+    @Transactional
+    public void insert(Role role) {
+        if (roleMapper.insert(role) > 0) {
+            Role newOne = roleMapper.selectByName(role.getName());
+            if (newOne != null) {
+                roleMapper.insertPermissionList(newOne.getRid(), role.getPermissions());
+                return;
+            }
+            throw new InvalidException("未知错误，角色插入失败");
+        }
+        if (roleMapper.selectByName(role.getName()) != null) {
+            throw new InvalidException("该角色已存在");
+        }
+        throw new InvalidException("未知错误，角色插入失败");
+    }
+
+    @Override
+    @Transactional
+    public void update(Role role) {
+        if (roleMapper.selectById(role.getRid()) != null) {
+            roleMapper.update(role);
+            roleMapper.deletePermissionNotInList(role.getRid(), role.getPermissions());
+            roleMapper.insertPermissionList(role.getRid(), role.getPermissions());
+            return;
+        }
+        throw new NotFoundException("未找到该角色");
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long rid) {
+        roleMapper.deletePermissionsById(rid);
+        roleMapper.delete(rid);
+    }
+
+    @Override
+    @Transactional
+    public void deleteList(List<Long> rids) {
+        roleMapper.deletePermissionsByIdList(rids);
+        roleMapper.deleteList(rids);
+    }
+}
--- bhs-system/src/main/java/top/brookezb/bhs/service/RoleService.java
+++ bhs-system/src/main/java/top/brookezb/bhs/service/RoleService.java
@@ -1,7 +1,22 @@
 package top.brookezb.bhs.service;
 
+import top.brookezb.bhs.model.Role;
+
+import java.util.List;
+
 /**
  * @author brooke_zb
  */
 public interface RoleService {
+    Role selectById(Long rid);
+
+    List<Role> selectAll();
+
+    void insert(Role role);
+
+    void update(Role role);
+
+    void delete(Long rid);
+
+    void deleteList(List<Long> rids);
 }
