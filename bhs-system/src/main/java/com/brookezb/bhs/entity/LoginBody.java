ec560ce6ac5204e759e6176d42092c387d7e7d02
brooke_zb <brooke_zb@qq.com>
2022-02-09 01:38:16 +0800

    remove sensitive information when logging

--- bhs-system/src/main/java/com/brookezb/bhs/entity/LoginBody.java
+++ bhs-system/src/main/java/com/brookezb/bhs/entity/LoginBody.java
@@ -2,12 +2,22 @@ package com.brookezb.bhs.entity;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author brooke_zb
  */
 @Data
 public class LoginBody {
+    @NotNull
     private String username;
+
+    @NotNull
     private String password;
+
     private boolean rememberMe = false;
+
+    public String toString() {
+        return "LoginBody(username=" + this.getUsername() + ", password=******" + ", rememberMe=" + this.isRememberMe() + ")";
+    }
 }
--- bhs-system/src/main/java/com/brookezb/bhs/entity/UpdatePasswordBody.java
+++ bhs-system/src/main/java/com/brookezb/bhs/entity/UpdatePasswordBody.java
@@ -2,11 +2,20 @@ package com.brookezb.bhs.entity;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author brooke_zb
  */
 @Data
 public class UpdatePasswordBody {
+    @NotNull
     private String oldPassword;
+
+    @NotNull
     private String newPassword;
+
+    public String toString() {
+        return "UpdatePasswordBody(oldPassword=******, newPassword=******)";
+    }
 }
