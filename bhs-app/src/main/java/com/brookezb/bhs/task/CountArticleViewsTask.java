f061eade68d4fb0261c8b1a112196bfd1811d22c
brooke_zb <brooke_zb@qq.com>
2022-02-17 15:51:46 +0800

    optimize task logging

--- bhs-app/src/main/java/com/brookezb/bhs/task/CountArticleViewsTask.java
+++ bhs-app/src/main/java/com/brookezb/bhs/task/CountArticleViewsTask.java
@@ -17,11 +17,13 @@ import org.springframework.stereotype.Component;
 public class CountArticleViewsTask {
     private RedisUtils redisUtils;
     private ArticleService articleService;
+    private int count = 0;
 
     @Scheduled(cron = "${task.cron.count-article-views:@daily}")
     public void countArticleViews() {
         log.info("开始统计文章阅读量");
 
+        this.count = 0;
         redisUtils.getKeys(AppConstants.REDIS_ARTICLE_VIEW_COUNT + "*")
                 .forEach(key -> {
                     Integer read = redisUtils.getSetSize(key);
@@ -30,8 +32,9 @@ public class CountArticleViewsTask {
                         log.info("- 文章阅读量持久化成功，文章ID={}, 阅读增量={}", aid, read);
                     }
                     redisUtils.delete(key);
+                    count++;
                 });
 
-        log.info("完成统计文章阅读量");
+        log.info("完成统计，共更新了{}篇文章", count);
     }
 }
