1d99600da3ae58eb283bf054df0bad1de8b2c066
brooke_zb <brooke_zb@qq.com>
2022-03-28 14:59:51 +0800

    fix sql order by glitch

--- bhs-common/src/main/java/com/brookezb/bhs/utils/PageUtils.java
+++ bhs-common/src/main/java/com/brookezb/bhs/utils/PageUtils.java
@@ -2,6 +2,7 @@ package com.brookezb.bhs.utils;
 
 import com.brookezb.bhs.entity.PageSelector;
 import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 
 import java.util.List;
 
@@ -12,6 +13,9 @@ import java.util.List;
 public class PageUtils {
     public static <T> List<T> selectPage(List<Long> ids, PageSelector selector, Class<T> clazz) {
         if (ids instanceof Page page) {
+            if (page.getOrderBy() != null) {
+                PageHelper.orderBy(page.getOrderBy());
+            }
             List<?> result = selector.select(page);
             page.clear();
             page.addAll(result);
